cd6a3445067c7ca51c400767da9b41f8b9d32393
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-09 23:32:39 -0500

    adding negatove index slice

---
+++ 9.py
@@ -0,0 +1,6 @@
+# Question: Complete the script so that it prints out a list slice containing the last three items of list letters .
+# Expected output: ['h', 'i', 'j'] 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print(letters[-3:])
