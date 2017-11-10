fcdf1a7d395832f008fb3416ee2bc0e963a0eaf5
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-10 00:02:13 -0500

    adding results out put

---
+++ 12.py
@@ -0,0 +1,6 @@
+# Complete the script so that it produces the expected output. Please use my_range  as input data.
+# Expected output: 
+# [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200] 
+
+my_range = range(1, 201)
+print (my_range[::10])
