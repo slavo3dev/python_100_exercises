73805cde01626d664c09711e8bdc2242291d1fc0
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-10 08:03:54 -0500

    adding filtering and making new dic

--- 21.py
+++ 21.py
@@ -1,13 +1,23 @@
 # Question: Filter the dictionary by removing all items with a value of greater than 1.
-
 d = {"a": 1, "b": 2, "c": 3}
-
-for k, v in d.items():
-    if v > 1:
-        del d[k]
-    else:
-        print(d)
-
-
+d = dict((key, value) for key, value in d.items() if value <= 1)
+print(d)
 
 # Expected output: {'a': 1}  
+
+'''
+Here we're using a dictionary comprehension. The comprehension is the expression inside dict() . The comprehension iterates through the existing dictionary items and if an item is less or equal to 1,the item is added to a new dict. This new dict is assigned to the existing variable d  so we end up with a filtered dictionary in d.
+'''
+
+
+c = {"a": 1, "b": 2, "c": 3}
+c_filter = {}
+for k, v in c.items():
+    if v <= 1:
+        c_filter[k] = v
+
+print(c_filter)
+    
+
+
+        
