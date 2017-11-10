ed1dc7ce861daeb4af52e5f4516aea4c59bfc3e2
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-10 09:08:36 -0500

    import math

--- 29.py
+++ 29.py
@@ -2,6 +2,7 @@
 Question:  Please write a function that calculates liquid volume in a sphere using the following formula. The radius r  is always 10, so consider making it a default parameter.
 You can then test your solution by passing 2 for h and you should get the expected output.
 '''
+from math import pi 
 
 def liquid_volume(h, r=10, pi=3.14):
     l_volume = ((4*pi*(r**3))/3) - (pi*(h**2)*(3*r-h)/3)
