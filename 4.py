31406b06961bcae2d625808f7c4d04c90e1daccd
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-09 23:17:53 -0500

    answering the question

--- 4.py
+++ 4.py
@@ -1,3 +1,10 @@
 
 
-Question: Fix the last line so that it outputs the sum of 1 and 2.
+# Question: Fix the last line so that it outputs the sum of 1 and 2.
+# expected output is: #
+
+a = "1" # shoud convert to int 
+a = int(a)
+b = 2
+
+print(a + b)
