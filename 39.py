c7536d854053d2d626b66f003ce7a3b1a2afb86b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 08:54:55 -0500

    wrong attribute

--- 39.py
+++ 39.py
@@ -1,6 +1,8 @@
 # Question: The script is supposed to output the cosine of angle 1 radian, but instead it is throwing an error. Please fix the code so that it prints out the expected output.
 
 import math
-print(math.cosine(1))
+# print(math.cosine(1))
+
+print (math.cos(1))
 
 # Expected output: 0.5403023058681397                                                                 
