275a211a829a66a06a26dd6870d650254a9b22ca
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 18:26:33 -0500

    slice the wrong number

--- 44.py
+++ 44.py
@@ -10,6 +10,6 @@ import string
 file_name = 'word5.text'
 
 with open(file_name, 'w') as f:
-    for l1, l2, l3 in zip(string.ascii_lowercase[0::2], string.ascii_lowercase[1::2], string.ascii_lowercase[2::2]):
+    for l1, l2, l3 in zip(string.ascii_lowercase[0::3], string.ascii_lowercase[1::3], string.ascii_lowercase[2::3]):
         f.write(l1 + l2 + l3 + '\n')
 
