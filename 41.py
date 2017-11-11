4b6029117efcc622df7357642f8383f80e99c7da
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 09:15:36 -0500

    typo

---
+++ 41.py
@@ -0,0 +1,16 @@
+# Question: Create a script that generates a text file with all letters of English alphabet inside it, one letter per line.
+
+import string
+
+file_text = 'word3.txt'
+
+def addin_strings(str_name):
+    with open(file_text, 'w') as f:
+        f.write(str)
+        content = file_text
+        print(content)
+
+my_str = string.ascii_lowercase
+
+addin_strings(my_str)
+    
