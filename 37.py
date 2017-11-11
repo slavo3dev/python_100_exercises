dc2a2d884499c9a5408142576350604d930ec104
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 08:44:31 -0500

    adding 2nd Solution

--- 37.py
+++ 37.py
@@ -2,7 +2,6 @@
 Question: Create a function that takes a text file as input and returns the number of words contained in the text file. Please take into consideration that some words can be separated by a comma with no space. For example "Hi,it's me." would need to be counted as three words. For your convenience, you can use the text file in the attachment.
 '''
 import re 
-
 my_str = ''
 file_name = 'words2.txt'
 
@@ -14,3 +13,13 @@ def count_str(cnt_str):
     print ('Your string has: ', len(cnt_str) - 1, ' number of words')
 
 count_str(my_str)
+
+# 2nd solution
+
+def count_words_re(filepath):
+    with open(filepath, 'r') as file:
+        string = file.read()
+        string_list = re.split(",| ", string)
+        return len(string_list)
+ 
+print(count_words_re("words2.txt"))
