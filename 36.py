1e9fd3b01aa7d72d455b90eb9b995fdeea2ed8ad
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 08:05:29 -0500

    split not slice

--- 36.py
+++ 36.py
@@ -1,1 +1,21 @@
 # Question: Please download the words1.txt file from the attachment and then create a Python function that takes a text file as input and returns the number of words contained in the text file.
+
+text_file = ('words1.txt')
+
+my_str = ''
+
+with open(text_file) as file_obj:
+    content = file_obj.read()
+    print (content)
+
+my_str = content
+
+
+def count_str(str_use):
+    print('Your str lenght is: ', len(str_use.split()))
+
+count_str(my_str)
+
+
+
+    
