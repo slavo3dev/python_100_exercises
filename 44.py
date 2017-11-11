a10d9c8486d523b81f5f69c6e83d70f01573c4ef
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 18:13:35 -0500

    new projects

---
+++ 44.py
@@ -0,0 +1,10 @@
+'''
+Create a script that generates a file where all letters of English alphabet are listed three in each line. The inside of the text file would look like:
+abc
+def
+ghi
+and so on.
+'''
+
+file_name = 'word5.text'
+
