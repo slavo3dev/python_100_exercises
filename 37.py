4ea8ba711664d517288fa54bc1505ec84913760b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 08:15:42 -0500

    print my str

--- 37.py
+++ 37.py
@@ -2,3 +2,9 @@
 Question: Create a function that takes a text file as input and returns the number of words contained in the text file. Please take into consideration that some words can be separated by a comma with no space. For example "Hi,it's me." would need to be counted as three words. For your convenience, you can use the text file in the attachment.
 '''
 
+file_name = 'words2.txt'
+
+with open(file_name) as f:
+    content = f.read()
+my_str = content.split()
+print (my_str)
