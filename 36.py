6574662b5c594109efd8ea1217c8287a71ed20cf
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 07:54:50 -0500

    adding new files one is a text

---
+++ 36.py
@@ -0,0 +1,1 @@
+# Question: Please download the words1.txt file from the attachment and then create a Python function that takes a text file as input and returns the number of words contained in the text file.
