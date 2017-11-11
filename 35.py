bc96eb08d14b2060503bbdd6cbeff8b05df235b5
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 07:50:50 -0500

    simple func

---
+++ 35.py
@@ -0,0 +1,10 @@
+# Create a function that takes any string as input and returns the number of words for that string.
+
+a = input('What is you name? ')
+b = input('How old are you? ')
+
+def info():
+    print ('Your info is ', a," ",str(b))
+
+info()
+    
