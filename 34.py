672155c19177cfd53e5fd5a0d06b5f8350ccdd76
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-11 07:46:39 -0500

    local variable

---
+++ 34.py
@@ -0,0 +1,10 @@
+# Question: The following script throws a NameError  in the last line saying that c  is not defined. Please fix the function so that there is no error and the last line is able to print out the value of c  (i.e. 1 ).
+
+def foo(): 
+    c = 1 
+    return c 
+foo()
+c = 1 
+print(c)
+
+# c is not defined becuse variable is inside the fucntion foo, and c is a local var
