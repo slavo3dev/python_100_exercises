db2b41ef0b402bfae9984af81780688be98af137
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 07:35:59 -0500

    making code nice

---
+++ 56.py
@@ -0,0 +1,23 @@
+''' 
+Question: Store the dictionary in a json file.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+'''
+d = {
+                "employees":
+                              [
+                                {"firstName": "John", "lastName": "Doe"},
+                                {"firstName": "Anna", "lastName": "Smith"},
+                                {"firstName": "Peter", "lastName": "Jones"}
+                                ],
+                 "owners":    
+                              [
+                                  {"firstName": "Jack", "lastName": "Petter"},
+                                  {"firstName": "Jessy", "lastName": "Petter"}
+                                ]
+    }
