a9aa703e84eb5303c437a6e7b42c7ee47c9a53c7
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 09:33:17 -0500

    adding files

--- 58.py
+++ 58.py
@@ -11,7 +11,9 @@ with open(json_file, 'r+') as target:
     data_file = json.load(target)
     pprint(data_file)
     data_file['employees'].append(dict(firstName = 'Slavo', lastName = 'Popovic'))
+    target.seek(0)
     json.dump(data_file, target, indent=4, sort_keys=True)
+    target.truncate()
     
 
 
