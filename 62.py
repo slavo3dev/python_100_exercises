798e650d2b2fc4128e0edb9fe279990994e1fc4b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 18:18:46 -0500

    count time

---
+++ 62.py
@@ -0,0 +1,22 @@
+''''
+Question: Create a program that once executed the program prints Hello  instantly first, then it prints it after 1 second, then after 2, 3, 4, and so on the interval increases between prints.
+Expected output: 
+
+...
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+...
+
+'''
+
+from time import sleep
+
+count = 0
+while count < 20:
+    print ("Hello Slavo after ", count + 1, 's !!')
+    sleep(count + 1)
+    count = count + 1 
