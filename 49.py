05b791d354318a7023314a3fdcabd0f9f87ea233
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 07:02:13 -0500

    fix the error

---
+++ 49.py
@@ -0,0 +1,13 @@
+''' 
+Question: The code is supposed to get some input from the user, but instead it produces an error. Please try to understand the error and then fix it.
+
+pass = input("Please enter your password: ") 
+
+Note: Please use raw_input  instead of input if you are on Python 2. For Python 3 input  is fine.
+
+'''
+
+psw = input("Please enter your password: ")
+print(psw)
+
+# we can not use Python key_words to declare a variable
