5527cffb39869d06b5671525d6d67ba8fd2bba2e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 07:17:22 -0500

    writting ans

--- 52.py
+++ 52.py
@@ -12,3 +12,5 @@ Expected output: Your first name is John and your second name is Smith
 firstname = input("Enter first name: ")
 secondname = input("Enter second name: ")
 print("Your first name is %s and your second name is %s" % (firstname, secondname))
+
+# variables needs to be inside the ()
