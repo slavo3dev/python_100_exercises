e811c1b61067d5703f59b3cc676dfda091a0455a
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 18:45:09 -0500

    building the translator

---
+++ 66.py
@@ -0,0 +1,20 @@
+'''
+
+Question: Create an English to Portuguese translation program.
+
+The program takes a word from the user as input and translates it using the following dictionary as a vocabulary source.
+
+d = dict(weather = "clima", earth = "terra", rain = "chuva") 
+
+Expected output: 
+
+Enter word: earth
+terra
+
+'''
+d = dict(weather = "clima", earth = "terra", rain = "chuva")
+def vocabulary(word):
+    return d[word]
+ 
+word = input("Enter word: ")
+print(vocabulary(word))
