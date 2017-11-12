7553fe8b42b37445f4f48bbd12e514c0b094ef71
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 06:25:40 -0500

    other solution

--- 45.py
+++ 45.py
@@ -1,6 +1,6 @@
 # Please create a script that generates 26 text files named a.txt, b.txt, and so on up to z.txt. Each file should contain a letter reflecting its filename. So, a.txt will contain letter a, b.txt will contain letter b and so on.
 
-import string
+import string,os
 
 str = string.ascii_lowercase
 
@@ -8,3 +8,11 @@ for letter in str:
     with open('test_case/' + letter + '.txt', 'w') as f:
         f.write('Slavo this is: ' + letter)
 
+
+# 2 solution
+
+if not os.path.exists('letters'):
+    os.makedirs('letters')
+for letter in str:
+    with open('letters/' + letter + '.txt', 'w') as f:
+        f.write("this is other solution: " + letter)
