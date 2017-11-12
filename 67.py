7dba63db431e913587dd2dcb0c17528cd5f6537e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 18:58:37 -0500

    didnt call the function

--- 67.py
+++ 67.py
@@ -1,15 +1,37 @@
 ''''
 
-Question: Create an English to Portuguese translation program.
+Question: Create an English to Serbian translation program.
 
-The program takes a word from the user as input and translates it using the following dictionary as a vocabulary source. In addition, try to return the message "We couldn't find that word!" when the user enters a word that is not in the dictionary.
+The program takes a word from the user as input and translates it using the following dictionary as a vocabulary source.
 
-d = dict(weather = "clima", earth = "terra", rain = "chuva") 
+d = dict(weather = "vreme", earth = "zemlja", rain = "kisa") 
 
 Expected output: 
 
-Enter word: hello
+Enter word: earth
+zemlja
 That word doesn't exist!
 
 '''
+print('Simple Serbian Translator!!!')
 
+d = dict(weather = "vreme", earth = "zemlja", rain = "kisa")
+
+def translation(word):
+    return d[word]
+
+w = input('Please choose word (weather, earth, rain): ')
+active = True
+
+while active:
+    print('Please print q if you want to exit!!')
+    w = input('Please choose word (weather, earth, rain): ')
+    if w != 'q':
+        try:
+            print("Your word in Serbian means: ", translation(w))
+        except:
+            print("This word doesn't exist!!\nPlease try again!!")
+    else:
+        active = False
+
+print ('Thank You for Choosing MimiCom24!!!!')
