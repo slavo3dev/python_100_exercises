d7dba200d9b27d67e0f5c66478aea7f1134f0dd5
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-12 07:54:21 -0500

    adding json load and pprint to stype json files

---
+++ 57.py
@@ -0,0 +1,18 @@
+'''
+Question: Please download the file in the attachment and use Python to print out its content.
+
+Expected output: 
+
+{'employees': [{'firstName': 'John', 'lastName': 'Doe'},
+               {'firstName': 'Anna', 'lastName': 'Smith'},
+               {'firstName': 'Peter', 'lastName': 'Jones'}],
+ 'owners': [{'firstName': 'Jack', 'lastName': 'Petter'},
+            {'firstName': 'Jessy', 'lastName': 'Petter'}]}
+
+'''
+
+import json, pprint
+
+with open('company1.json', "r") as f:
+    content = json.load(f)
+    pprint.pprint (content)
