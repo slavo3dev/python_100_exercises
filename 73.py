dc83250f466e9aadac55eb7082c5c2d86b2f91ba
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-13 12:06:42 -0500

    import pandas

--- 73.py
+++ 73.py
@@ -1,1 +1,7 @@
 # Multiply the text values in the URL and export the output to the new file
+
+import pandas
+ 
+data = pandas.read_csv("http://www.pythonhow.com/data/sampledata.txt")
+data_2 = data * 2
+data_2.to_csv("sampledata_x_2.txt", index=None)
