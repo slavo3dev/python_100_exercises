1001e33b4ffb60f3f1f183a1edc825424154dde2
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-13 11:16:21 -0500

    need to install requests module

--- 70.py
+++ 70.py
@@ -2,4 +2,14 @@
 
 Question: Print out the text of this file http://www.pythonhow.com/data/universe.txt. Please don't manually download the file. Let Python do all the work.
 
+
+import requests
+r = requests.get("http://www.pythonhow.com/data/universe.txt")
+text_file = r.text
+print(text_file)
 '''
+
+import requests
+response = requests.get("http://www.pythonhow.com/data/universe.txt")
+text = response.text
+print(text)
