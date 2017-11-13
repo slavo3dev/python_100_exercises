8261c3d5a50261ae4138b45a245556c77aef8f03
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-13 11:40:57 -0500

    doing the project in jupyter

--- 71.py
+++ 71.py
@@ -1,3 +1,6 @@
 # Question: Count the number of "a" characters in this text file: http://www.pythonhow.com/data/universe.txt
 
-
+import requests 
+r = requests.get('http://www.pythonhow.com/data/universe.txt')
+text = r.text
+file_name = "word6.txt"
