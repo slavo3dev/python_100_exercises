da3b1bb01444a818e3b0633ad7d73789dcc9b306
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-13 11:43:42 -0500

    adding answare

--- 71.py
+++ 71.py
@@ -1,6 +1,22 @@
 # Question: Count the number of "a" characters in this text file: http://www.pythonhow.com/data/universe.txt
 
+# download a txt document, and make a var for a new file
 import requests 
 r = requests.get('http://www.pythonhow.com/data/universe.txt')
 text = r.text
 file_name = "word6.txt"
+# write a new txt file and save the content from the internet
+with open(file_name, 'w') as f:
+    f.write(text)
+# load a new file and save text as string
+text_content = ''
+with open('word6.txt', 'r') as f:
+    text_content = f.read()
+    print(text_content)
+# looping all the letters and count only when is letter a 
+count = 0
+for letter in text_content:
+    if letter == 'a':
+        count += 1
+        
+print("Count number of letter 'a' is: ", count
