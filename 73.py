4aecff690724f49a0403d9850c4d92e55c8fcc1b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-13 12:08:31 -0500

    adding padnas

--- 73.py
+++ 73.py
@@ -4,4 +4,4 @@ import pandas
  
 data = pandas.read_csv("http://www.pythonhow.com/data/sampledata.txt")
 data_2 = data * 2
-data_2.to_csv("sampledata_x_2.txt", index=None)
+data_2.to_csv("word7_pandas.txt", index=None)
