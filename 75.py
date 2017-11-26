6af68d166083f01fce3e39fcd54d30d40e03c39d
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-26 07:40:01 -0500

    adding new solution

--- 75.py
+++ 75.py
@@ -11,4 +11,12 @@ f_y = list(f_load['y'])
 print(f_x)
 print(f_y)
 
+plt.scatter(f_x, f_y, s=40)
+plt.show()
+
+# Solution 2
+
+data = pandas.read_csv("http://www.pythonhow.com/data/sampledata.txt")
+data.plot(x='x', y='y', kind='scatter')
+plt.show()
 
