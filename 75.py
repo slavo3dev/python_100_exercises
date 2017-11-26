0e36ae4b6834d599b42b4a415eed4934b1cb9da2
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-26 07:35:57 -0500

    removing scatter

--- 75.py
+++ 75.py
@@ -2,14 +2,13 @@
 Plot text file 
 '''
 
-import matplotlib
+import matplotlib.pyplot as plt
 import pandas
-
-
 f_load = pandas.read_csv('75.txt')
 print(f_load)
 f_x = list(f_load['x'])
 f_y = list(f_load['y'])
 print(f_x)
 print(f_y)
+
 
