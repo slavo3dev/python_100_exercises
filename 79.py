807fe8edf295c0843c938ff1569ca685132b8579
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-27 06:49:01 -0500

    password input

--- 79.py
+++ 79.py
@@ -1,3 +1,18 @@
 '''
 Paassword checker
+
+1. at leas one number
+2. one upprcase letter
+3. one character
+
 '''
+
+import random
+ 
+characters = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+chosen = random.sample(characters, 6)
+print (chosen)
+password = "".join(chosen)
+print(password)
+
+user_password: input('Enter Password?' )
