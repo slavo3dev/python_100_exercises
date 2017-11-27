7fbba5ebae331d5bb909286a2526a276a98be0e6
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-27 06:44:23 -0500

    fixing and adding solution

--- 78.py
+++ 78.py
@@ -2,15 +2,10 @@
 Creat a program that Generates a password
 '''
 
-from random import random
-import string
-print (string.ascii_letters)
-num = random()
-print(int(20*num))
-l = list(string.ascii_letters)
-print(l)
-print(len(l))
 
-while i < 10:
-    print(l[(52*random())].join())
-    i = i + 1
+import random
+ 
+characters = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+chosen = random.sample(characters, 6)
+password = "".join(chosen)
+print(password)
