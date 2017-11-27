21534444a996bb3a846468688e11dcad86cc5216
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-27 06:45:17 -0500

    printing choosen

--- 78.py
+++ 78.py
@@ -7,5 +7,6 @@ import random
  
 characters = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 chosen = random.sample(characters, 6)
+print (chosen)
 password = "".join(chosen)
 print(password)
