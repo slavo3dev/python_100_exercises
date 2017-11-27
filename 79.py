c5cb16d4d5bd27ae3740e9b96e0350fa2acc648e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-27 06:58:36 -0500

    adding conditions

--- 79.py
+++ 79.py
@@ -14,5 +14,17 @@ chosen = random.sample(characters, 6)
 print (chosen)
 password = "".join(chosen)
 print(password)
+numbers = '01234567890'
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+character = '!@#$%^&*()?'
 
-user_password: input('Enter Password?' )
+
+while True:
+    user_password: input('Enter Password?' )
+    for n in user_password:
+        if numbers == n and uppercase == n and character == n and len(password) == len(user_password):
+            True
+            print('Password is fine, well done')
+        else:
+            print('Password is not good, please try again!')
+        
