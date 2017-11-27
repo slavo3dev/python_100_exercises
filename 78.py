bcdff52b13d97f0c13a48d3f91654f1162898457
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-27 06:36:15 -0500

    adding random number and join method

--- 78.py
+++ 78.py
@@ -4,8 +4,13 @@ Creat a program that Generates a password
 
 from random import random
 import string
-
 print (string.ascii_letters)
 num = random()
-
 print(int(20*num))
+l = list(string.ascii_letters)
+print(l)
+print(len(l))
+
+while i < 10:
+    print(l[(52*random())].join())
+    i = i + 1
