ec6995810fb42fee5d4377fb11174d3dd8b0c46b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-28 07:55:44 -0500

    fixing code to elif statement

--- 81.py
+++ 81.py
@@ -1,4 +1,25 @@
 '''
 Creat a input for username - if username exits in date ask for a password
+
 '''
 
+
+
+
+# password checker
+while True:
+    psw = input("Enter new password: ")
+    if any(i.isdigit() for i in psw) and any(i.isupper() for i in psw) and len(psw) >= 5:
+        print("Password is fine")
+        break
+    elif any(i.isdigit() for i in psw) and any(i.isupper() for i in psw):
+        print ("Your don't have 6 characters in password!")
+        pass
+    elif any(i.isupper() for i in psw) and len(psw) >= 5:
+        print ("You need to have a digit and Character!")
+        pass
+    elif any(i.isdigit() for i in psw) and len(psw) >= 5:
+        print ('Your password need at least one upper case letter!')
+        pass
+    else:
+        print("Passowrd is not fine!(Missing at least one upper case letter,  symbol, charater ")
