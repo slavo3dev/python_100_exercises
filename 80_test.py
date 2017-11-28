6869cd7f9c5ed9a2351e023c56359dcead2a5d12
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-28 07:46:16 -0500

    adding code

--- 80_test.py
+++ 80_test.py
@@ -5,10 +5,10 @@ while True:
         break
     if any(i.isdigit() for i in psw) and any(i.isupper() for i in psw):
         print ("Your don't have 6 characters in password!")
-        continue
+        pass
     if any(i.isupper() for i in psw) and len(psw) >= 5:
-        print ("You need to have a digits!")
-        continue
+        print ("You need to have a digit and Character!")
+        pass
     if any(i.isdigit() for i in psw) and len(psw) >= 5:
         print ('Your password need at least one upper case letter!')
         pass
