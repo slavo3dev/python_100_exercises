9f23560784c825ac996ce95e5779c1f93d24b6a1
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-28 07:39:27 -0500

    if statements

---
+++ 80_test.py
@@ -0,0 +1,17 @@
+while True:
+    psw = input("Enter new password: ")
+    if any(i.isdigit() for i in psw) and any(i.isupper() for i in psw) and len(psw) >= 5:
+        print("Password is fine")
+        break
+    if any(i.isdigit() for i in psw) and any(i.isupper() for i in psw):
+        print ("Your don't have 6 characters in password!")
+        continue
+    if any(i.isupper() for i in psw) and len(psw) >= 5:
+        print ("You need to have a digits!")
+        continue
+    if any(i.isdigit() for i in psw) and len(psw) >= 5:
+        print ('Your password need at least one upper case letter!')
+        pass
+    else:
+        print("Passowrd is not fine!(Missing at least one upper case letter,  symbol, charater ")
+'''
