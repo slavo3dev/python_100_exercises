f08c9bb3dbea003ce897b130bc87bc3cc81f1ab0
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-29 11:12:55 -0500

    python solutions

--- 83.py
+++ 83.py
@@ -7,3 +7,13 @@ import screeninfo
 from screeninfo import get_monitors
 for m in get_monitors():
     print(str(m))
+
+
+# Solutions
+
+from screeninfo import get_monitors
+ 
+width=get_monitors()[0].width
+height=get_monitors()[0].height
+ 
+print("Width: %s,  Height: %s" % (width, height))
