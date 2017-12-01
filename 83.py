31b85458a2248e502c9c852c736eade6f202d816
slavo@mimicom24.com <slavo@mimicom24.com>
2017-11-30 21:38:39 -0500

    adding comment

--- 83.py
+++ 83.py
@@ -2,6 +2,7 @@
 Write a script that detects and prints out your monitor resolution
 '''
 # Solutions
+# code its not working on iOS envitoment
 from screeninfo import get_monitors
 for m in get_monitors('osx'):
     print(str(m))
