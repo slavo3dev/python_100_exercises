f63cc1054d790910837625e3cd8bef0d9fd3dbc1
slavo@mimicom24.com <slavo@mimicom24.com>
2017-11-30 21:36:30 -0500

    trying to run code on osx

--- 83.py
+++ 83.py
@@ -2,8 +2,6 @@
 Write a script that detects and prints out your monitor resolution
 '''
 # Solutions
-
 from screeninfo import get_monitors
-
-for m in get_monitors():
+for m in get_monitors('osx'):
     print(str(m))
