8787180e92c4426998cc84ce2c03935be35a57ae
slavo@mimicom24.com <slavo@mimicom24.com>
2017-11-30 21:34:44 -0500

    adding new file

--- 83.py
+++ 83.py
@@ -1,19 +1,9 @@
 '''
 Write a script that detects and prints out your monitor resolution
 '''
-
-import screeninfo
-
-from screeninfo import get_monitors
-for m in get_monitors():
-    print(str(m))
-
-
 # Solutions
 
 from screeninfo import get_monitors
- 
-width=get_monitors()[0].width
-height=get_monitors()[0].height
- 
-print("Width: %s,  Height: %s" % (width, height))
+
+for m in get_monitors():
+    print(str(m))
