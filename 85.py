9fe89c4f4d5233c1c2f5c6206e22075f1ad00ac0
Slavoljub Popovic <slavo@mimicom24.com>
2017-12-03 08:47:10 -0500

    adding comment

---
+++ 85.py
@@ -0,0 +1,11 @@
+'''
+Please download the attached countries_raw.txt file. 
+The file contains the list of country names, 
+but it also contains some unnecessary text among the countries. 
+
+Please clean the list with Python by generating 
+a new text file that contains a flawless list of country names
+ without any other text or break lines in it. 
+ The new file content should look like in the expected output.
+'''
+
