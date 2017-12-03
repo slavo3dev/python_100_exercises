4f8732ba4459894994f95ea0331517ef69a53654
Slavoljub Popovic <slavo@mimicom24.com>
2017-12-03 10:04:37 -0500

    trying to appent missing items to a list

--- 87.py
+++ 87.py
@@ -1,8 +1,12 @@
 '''
 Adding missing data to a file to a data
 '''
-checklist = ["Portugal", "Germany", "Spain"]
+
+s = ["Portugal", "Germany", "Spain"]
 with open ('87_text.txt', 'r') as f:
     l_ctr = f.readlines()
 
 l_ctr = [i.strip('\n') for i in l_ctr]
+
+l_ctr = [i for i in s if i not in l_ctr]
+
