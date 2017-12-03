6cf082282c08deb273d8c6d2ba5d2eb2b4f3b7da
Slavoljub Popovic <slavo@mimicom24.com>
2017-12-03 10:10:08 -0500

    simple typo mistake

--- 87.py
+++ 87.py
@@ -3,7 +3,7 @@ Adding missing data to a file to a data
 '''
 
 s = ["Portugal", "Germany", "Spain"]
-s = [i + '\n' for i n s]
+s = [i + '\n' for i in s]
 
 with open ('87_text.txt', 'r') as f:
     l_ctr = f.readlines()
