51d4d40168a4ce7f57f4ad134823f2110d776210
Slavoljub Popovic <slavo@mimicom24.com>
2017-12-03 10:46:26 -0500

    adding good extension

--- 88.py
+++ 88.py
@@ -2,5 +2,9 @@
 Create a script that uses the attached countries_by_area.txt  
 file as data source and prints out the top 5 most densely populated countries.
 '''
+ 
 
-import pandas
+import pandas as pd 
+
+f = pd.read_csv('88.txt')
+print(f)
